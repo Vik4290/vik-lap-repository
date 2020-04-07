2448ad05bc2ee7fd90a82bda9c77240e4d475ccd
Vik4290 <vikrambakshiynr@gmail.com>
2020-04-07 22:58:01 +0530

    Changes for Extent Report4 Remove testng report

--- src/main/java/com/zom/pom/Utilites/Utilities.java
+++ src/main/java/com/zom/pom/Utilites/Utilities.java
@@ -32,9 +32,13 @@ public class Utilities extends Page
 		//new File(System.getProperty("user.dir") + "/reports/" + screenshotName));
 		//screenshotPath= System.getProperty("user.dir") + "/reports/" + screenshotName;
 
-		new File(System.getProperty("user.dir") + "\\target\\surefire-reports\\html\\" + screenshotName));
+/*		new File(System.getProperty("user.dir") + "\\target\\surefire-reports\\html\\" + screenshotName));
 		screenshotPath= System.getProperty("user.dir") + "\\target\\surefire-reports\\html\\" + screenshotName;
-	
+	*/
+		new File("C:\\Users\\Vikram\\Vik_POM\\com.zom.pom\\target\\surefire-reports\\html\\" + screenshotName));
+		screenshotPath= "C:\\Users\\Vikram\\Vik_POM\\com.zom.pom\\target\\surefire-reports\\html\\" + screenshotName;
+		
+		
 	}
 
 	@DataProvider(name="dp")
